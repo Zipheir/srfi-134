483b737a99e3373755949af552790f8c96319b3b
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2020-11-30 12:19:05 -0500

    Fix indentation.

--- srfi-134.scm
+++ srfi-134.scm
@@ -1,32 +1,33 @@
 (module (srfi 134)
   (ideque ideque-tabulate ideque-unfold ideque-unfold-right
-          ideque? ideque-empty? ideque= ideque-any ideque-every
+   ideque? ideque-empty? ideque= ideque-any ideque-every
 
-          ideque-front ideque-add-front ideque-remove-front
-          ideque-back  ideque-add-back  ideque-remove-back
+   ideque-front ideque-add-front ideque-remove-front
+   ideque-back  ideque-add-back  ideque-remove-back
 
-          ideque-ref
-          ideque-take ideque-take-right ideque-drop ideque-drop-right
-          ideque-split-at
+   ideque-ref
+   ideque-take ideque-take-right ideque-drop ideque-drop-right
+   ideque-split-at
 
-          ideque-length ideque-append ideque-reverse
-          ideque-count ideque-zip
+   ideque-length ideque-append ideque-reverse
+   ideque-count ideque-zip
 
-          ideque-map ideque-filter-map
-          ideque-for-each ideque-for-each-right
-          ideque-fold ideque-fold-right
-          ideque-append-map
+   ideque-map ideque-filter-map
+   ideque-for-each ideque-for-each-right
+   ideque-fold ideque-fold-right
+   ideque-append-map
 
-          ideque-filter ideque-remove ideque-partition
+   ideque-filter ideque-remove ideque-partition
 
-          ideque-find ideque-find-right
-          ideque-take-while ideque-take-while-right
-          ideque-drop-while ideque-drop-while-right
-          ideque-span ideque-break
+   ideque-find ideque-find-right
+   ideque-take-while ideque-take-while-right
+   ideque-drop-while ideque-drop-while-right
+   ideque-span ideque-break
 
-          list->ideque ideque->list
-          generator->ideque ideque->generator
-          )
+   list->ideque ideque->list
+   generator->ideque ideque->generator
+   )
+
   (import (scheme)
           (chicken base)
           (srfi 1)
