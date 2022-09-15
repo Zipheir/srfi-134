0038240252951becec543595280976530b903133
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2022-09-15 11:26:53 -0400

    Tests: Import (srfi 134 extensions).

--- tests/run.scm
+++ tests/run.scm
@@ -2,6 +2,7 @@
         (chicken condition)
         (chicken random)
         (srfi 134)
+        (srfi 134 extensions)
         (only (srfi 1) take drop iota split-at list-tabulate any every)
         (only (srfi 41) stream-null stream-null? list->stream
                         stream->list)
